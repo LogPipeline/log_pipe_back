18d1ed240822c22d73174e122fac20a1766cfc7d
sky81219 <=>
2023-06-05 19:30:43 +0900

    refactor: architecture

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -9,6 +9,6 @@ python manage.py makemigrations dashboard
 python manage.py migrate --noinput
 
 echo "${0} : collect staticfile "
-python3 manage.py collectstatic --noinput
+python manage.py collectstatic --noinput
 
 gunicorn config.wsgi --bind 0.0.0.0:8000
